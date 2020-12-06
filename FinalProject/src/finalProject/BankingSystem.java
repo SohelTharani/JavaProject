21e2161b1650a6962c5b344be974d84df03b474f
Sohel Tharani - 616 <Owner@DESKTOP-B24ALDV>
2020-12-05 21:18:16 -0500

    Initial Commit Created the first class(Main Project) and a test class (testing connection with team members)

---
+++ FinalProject/src/finalProject/BankingSystem.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package finalProject;
+
+/**
+ * @author Sohel Tharani - 616
+ *
+ */
+public class BankingSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
---
+++ FinalProject/src/finalProject/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package finalProject;
+
+/**
+ * @author Owner
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		//@SohelTharani-616
+		System.out.println("I am testing the project initially.");
+
+	}
+
+}
