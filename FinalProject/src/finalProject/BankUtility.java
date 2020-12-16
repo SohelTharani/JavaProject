703f432d9429a777f76cd0954e627826763e54de
SohelTharani616 <SohelTharani616@DESKTOP-B24ALDV>
2020-12-15 23:38:32 -0500

    Created class called bank Added Menu for user that what he wants to do Also added submenu Create account on client's demand
    *Changes have to be made*

---
+++ FinalProject/src/finalProject/BankUtility.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package finalProject;
+
+/**
+ * @author Sohel Tharani - 616
+ *
+ */
+public class BankUtility {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
