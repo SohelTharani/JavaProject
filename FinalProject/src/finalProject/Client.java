52878e1906c4b217e9800f2d17e2fde8dbbd66f1
HariniSivakumar-495 <Sivakumar@LAPTOP-K58CIGP4>
2020-12-17 10:20:31 +0530

    I have updated client class.

--- FinalProject/src/finalProject/Client.java
+++ FinalProject/src/finalProject/Client.java
@@ -2,19 +2,49 @@
  * 
  */
 package finalProject;
-
+import java.io.Serializable;
+import java.util.ArrayList;
 /**
- * @author Sohel Tharani - 616
+ * @author HariniSivakumar- 495
  *
  */
-public class Client {
+public class Client implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int count =10001;
+	
+	String clientId;
+	String clientName;
+	String password;
+	ArrayList<Account>  accounts = new ArrayList<Account>();
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public ArrayList<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(ArrayList<Account> accounts) {
+		this.accounts = accounts;
+	}
+	
+	
+	
 
 	}
 
-}
