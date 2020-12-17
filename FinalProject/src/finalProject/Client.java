8e8e29d6b1cb5eb938794d64fac199afd9f486c6
SohelTharani-616 <SohelTharani-616@DESKTOP-B24ALDV>
2020-12-17 10:11:08 -0500

    Solved an error

--- FinalProject/src/finalProject/Client.java
+++ FinalProject/src/finalProject/Client.java
@@ -2,31 +2,21 @@
  * 
  */
 package finalProject;
-<<<<<<< HEAD
-import java.io.Serializable;
-import java.util.ArrayList;
-=======
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
->>>>>>> branch 'main' of https://github.com/SohelTharani-616/JavaProject
-/**
-<<<<<<< HEAD
- * @author HariniSivakumar- 495
-=======
- * @author HariniSivakumar-495
->>>>>>> branch 'main' of https://github.com/SohelTharani-616/JavaProject
+import java.io.Serializable;
+import java.util.ArrayList;
+
+ /* @author HariniSivakumar-495
  *
  */
-<<<<<<< HEAD
+
 public class Client implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-=======
->>>>>>> branch 'main' of https://github.com/SohelTharani-616/JavaProject
 
-<<<<<<< HEAD
 	static int count =10001;
 	
 	String clientId;
@@ -57,60 +47,5 @@ public class Client implements Serializable {
 	public void setAccounts(ArrayList<Account> accounts) {
 		this.accounts = accounts;
 	}
-	
-	
-	
-=======
-public class Client implements Serializable {
-	
-	// Variable de declaration
-	private static final long serialVersionUID = 1L;
->>>>>>> branch 'main' of https://github.com/SohelTharani-616/JavaProject
-
-	static int count =10001;
-	
-	String clientId;
-	String clientName;
-	String password;
-	
-	
-	ArrayList<Account>  accounts = new ArrayList<Account>();
-	// Returning  Client id 
-	public String getClientId() {
-		return clientId;
-	}
-<<<<<<< HEAD
-
-=======
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
-	
-	// Returning  Client name 
-	public String getClientName() {
-		return clientName;
-	}
-	public void setClientName(String clientName) {
-		this.clientName = clientName;
-	}
-	
-	// Client's password
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	//Client,S account
-	public ArrayList<Account> getAccounts() {
-		return accounts;
-	}
-	public void setAccounts(ArrayList<Account> accounts) {
-		this.accounts = accounts;
-	}
-	
-	
 	
 }
->>>>>>> branch 'main' of https://github.com/SohelTharani-616/JavaProject
