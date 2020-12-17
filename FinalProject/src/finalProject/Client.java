487d7aff49bc8cbee4bd0421cdc7295b42dc2252
HariniSivakumar-495 <Sivakumar@LAPTOP-K58CIGP4>
2020-12-17 11:04:28 +0530

    updating client class

--- FinalProject/src/finalProject/Client.java
+++ FinalProject/src/finalProject/Client.java
@@ -11,3 +11,41 @@ import java.util.ArrayList;
  *
  */
 
+public class Client implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	static int count =10001;
+	
+	String clientId;
+	String clientName;
+	String password;
+	ArrayList<Account>  accounts = new ArrayList<Account>();
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getClientName() {
+		return clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public ArrayList<Account> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(ArrayList<Account> accounts) {
+		this.accounts = accounts;
+	}
+	
+	
+	
+}
