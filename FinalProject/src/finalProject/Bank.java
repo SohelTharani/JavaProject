0f9d65b301953101441ad91f27ad4163e8f00e26
SohelTharani-616 <SohelTharani-616@DESKTOP-B24ALDV>
2020-12-16 23:48:23 -0500

    Created a class called Bank Added menu method that provides menu for user to perform Added Main Method that read files and client information from BankUtility.java Added sub-menu that provides further option to perform basis on the selection in menu method Also added the method for depositing the money, transfering to another account, updating account details and currently implementing Registration method

--- FinalProject/src/finalProject/Bank.java
+++ FinalProject/src/finalProject/Bank.java
@@ -3,26 +3,24 @@
  */
 package finalProject;
 
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.*; 
-import java.io.IOException;
+import javax.swing.*;  
 
 /**
  * @author Sohel Tharani - 616
  *
  */
 
-//@SohelTharani520- 1
-public class Bank 
+//@SohelTharani616
+public class Bank
 {
-	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
-
+	
 	//Create an ArrayList which will store account details
 	static  ArrayList<Account> ls = new ArrayList<Account>();
 	
@@ -32,10 +30,10 @@ public class Bank
 	static JFrame f =new JFrame(); 
 	
 	//Welcome Page aka menu page
-	static void menu() throws IOException {
-		
+	static void menu() throws IOException 
+	{
 		//Collect what user want to do from given options
-		String userInput = JOptionPane.showInputDialog(null, "Press 1 for Customer Login \n Press 2 for Banker Login \n Press 3 for Registration");
+		String userInput = JOptionPane.showInputDialog(null, "Press 1 for Customer Login \n Press 2 for Banker Login");
 		int option = Integer.parseInt(userInput);
 		
 		switch(option)
@@ -49,130 +47,185 @@ public class Bank
 			menu();
 				break;
 		case 3:
-			Registration(); //Redirect to Registration class 
+			Registration(); //Redirect to Registration class
+			menu();
 				break;
-		
 		}
 	}
-
+	
 	//@SohelTharani616
+	//Main Method
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		try { 
-					//Will ReadFiles from BankUtility.java file
-			// ReadClient from BankUtility.java file
+		
+		try {
+			ls = BankUtility.readAccountFromFile();					//ReadFiles from BankUtility.java file
+			clientList = BankUtility.readClientFromFile();			// ReadClient from BankUtility.java file
 			JOptionPane.showMessageDialog(null, "LAMBTON BANK");
-			menu();	//Goto Menu class
-			
+			menu();			//Goto Menu class
 		}catch(Exception  e) {
 			JOptionPane.showMessageDialog(null, "Operation failed due to inavlid input. Please start again.");
 			/*
-			 * try 
-			 * { 
+			 * try { 
 			 * 		menu(); 
-			 * }catch (IOException e1) 
-			 * { 
-			 * 		// TODO Auto-generated catch block
-			 * 		e1.print(); 
-			 * }
+			 * } catch (IOException e1) { // TODO Auto-generated catch block
+			 * e1.print(); }
 			 */
 		}
-		
 	}
 	
-	
-	//@SohelTharani616
-		private static void subMenu(String clientId, String accNoInput) throws IOException 
-		{
-			//Ask user for its choice that he want to do
-			String choice1 = JOptionPane.showInputDialog(null, "1. To Display Account Details \n "
-					+ "2. To Display Account Balance  \n"
-					+ "3. To Update Account Details  \n"
-					+ "4. To Draw Money \n"
-					+ "5. To Transfer Money to other account within Bank \n"
-					+ "6. To Pay Bills  \n"
-					+ "7. Go to Main Menu \n"
-					+ "8. To Exit \n"
-					+ "Please enter your choice :: ");
+	//SohelTharani616
+	//Provides SubMenu to user
+	private static void subMenu(String clientId, String accNoInput) throws IOException 
+	{
+		//Ask user for its choice that he want to do
+		String choice1 = JOptionPane.showInputDialog(null, "1. To Display Account Details \n "
+				+ "2. To Display Account Balance  \n"
+				+ "3. To Update Account Details  \n"
+				+ "4. To Deposit Money  \n"
+				+ "5. To Draw Money \n"
+				+ "6. To Transfer Money to other account within Bank \n"
+				+ "7. To Pay Bills  \n"
+				+ "8. Go to Main Menu \n"
+				+ "9. To Exit \n"
+				+ "Please enter your choice :: ");
 			
-			
-			int choice = Integer.parseInt(choice1);
-			
-			//Retrieve the account
-			Account acc  = getAccount(accNoInput);
-			switch(choice) {
-				case 1 :
-					printAccountDeatil(acc);		//Print Account Details
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 2 :
-					printAccountBalance(acc);
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 3 :
-					updateAccountDetail(acc);
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 4 :
-					withdrawMoney(acc);
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 5 :
-					transferMoney(acc);
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 6 :
-					payBills(acc);
-					subMenu(clientId,accNoInput);	//Again ask user that what he want to do
-					break;
-				case 7 :
-					menu();	//Goto Main menu
-					break;
-					
-				case 8 :
-					System.out.println("Exiting Thank you ...");
-					System.exit(0);					//Exit the system
-					
-			}
+		//Retrieve the account
+		int choice = Integer.parseInt(choice1);
+		Account acc  = getAccount(accNoInput);
+		switch(choice) {
+			case 1 :
+				printAccountDeatil(acc);		//Print Account Details
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 2 :
+				printAccountBalance(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 3 :
+				updateAccountDetail(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 4 :
+				depositMoney(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+				
+			case 5 :
+				withdrawMoney(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 6 :
+				transferMoney(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 7 :
+				payBills(acc);
+				subMenu(clientId,accNoInput);	//Again ask user that what he want to do
+				break;
+			case 8 :
+				menu();		//Goto Main menu
+				break;
+				
+			case 9 :
+				JOptionPane.showMessageDialog(null, "Exiting Thank you ...");
+				System.exit(0);				//Exit the system
+				
 		}
+	}
 		
-		//@SohelTharani616
-		//Add an account on client's demand
-		public static void addAccountForClient() {
+	//@SohelTharani616
+	//Deposit Money method
+	private static void depositMoney(Account acc) 
+	{	
+		//Ask for Deposit Amount
+		
+		
+		String amt = JOptionPane.showInputDialog(null, "Please enter the amount to deposit. ");
+		double amount = Double.parseDouble(amt);
+		acc.setBalance(acc.getBalance() +amount);
+		System.out.println("Deposit done");
+		System.out.println("New Balance ::" + acc.getBalance());
+		JOptionPane.showMessageDialog(null, "Deposite done \n "+"New Balance ::" + acc.getBalance());
 			
-			
-			String clientId = JOptionPane.showInputDialog(null, "Add Account for client \n"
-					+ "Enter a client id:");
-			
-			Client c =checkClientExist(clientId);		//Check wether user exists or not
-			if(c!=null) 
-			{
-				accountCreation(c); //if not then create account
-			}
-			else 
-			{
-				JOptionPane.showMessageDialog(null, "Client ID does exist");	 
-			}
-			
-		}
-		//@SohelTharani616
-		//Registration for new account
-		public static void Registration() throws IOException
+	}
+
+
+	//@SohelTharani616
+	//This method will transfer money to another account
+	private static void transferMoney(Account acc) 
+	{
+		// TODO Auto-generated method stub
+		
+		//Ask for account number
+		String accNo2 = JOptionPane.showInputDialog(null, "Please enter the account no to which money need to transfer.");
+		
+		Account ac2 = getAccount(accNo2);
+		
+		//Check for account
+		if(ac2 == null) 
 		{
-			
-			JOptionPane.showMessageDialog(null, "Registration");
-			
-			String clientName = JOptionPane.showInputDialog(null, "Enter a client:");	//Ask for clients ID
-			String password = JOptionPane.showInputDialog(null, "Enter a Password:");	//ask to enter a password
-			
-			Client client = new Client();		//Create a new client
-			//client.setPassword(password);		//Set password
-			//client.setClientId(++Client.count + "");	//setclient id
-			clientList.add(client);				//
-			//accountCreation(client);			
-			
+			JOptionPane.showMessageDialog(null, "Account do not exist");
+			return;
 		}
+
+		//Ask for amount
+		String amt = JOptionPane.showInputDialog(null, "Please enter the amount to transfer.");
+		double amount = Double.parseDouble(amt);
+			
+		//Check for balance availability. If available then transfer
+		if(acc.getBalance() > amount) 
+		{
+			acc.setBalance(acc.getBalance() -amount);
+			ac2.setBalance(ac2.getBalance() +amount);
+			JOptionPane.showMessageDialog(null, "Transfer done \n New Balance :: "+acc.getBalance());			
+		} 
+		else 
+		{
+			JOptionPane.showMessageDialog(null, "Low balance  can't be allowed to transfer");
+		}
+	}
+
+
+	//@SohelTharani616
+	//Update existing account details
+	private static void updateAccountDetail(Account ac) 
+	{
+		// TODO Auto-generated method stub
+		
+		//Ask for first name to update
+		String fName = JOptionPane.showInputDialog(null, "Please enter the first name. ");
+		ac.setfName(fName);
+		
+		//Ask for last name to update
+		ac.setlName(JOptionPane.showInputDialog(null, "Please enter the last name. "));
+		
+		//Ask for address to update
+		ac.setAddress(JOptionPane.showInputDialog(null, "Please enter the address. "));
+		
+		//Ask for phone number to update
+		ac.setPhoneNo(JOptionPane.showInputDialog(null, "Please enter the phone no. "));
+		
+		//Save details to file
+		BankUtility.saveClientToFile(clientList);
+		
+	}
+		
+	//@SohelTharani616
+	//This method is for registering a new user
+	public static void Registration() throws IOException
+	{
+		
+		JOptionPane.showMessageDialog(null, "Registration");
+		
+		//Accept Client Name & Password
+		String clientName = JOptionPane.showInputDialog(null, "Enter a client:");	
+		String password = JOptionPane.showInputDialog(null, "Enter a Password:");
+		
+		//Create  new client
+		Client client = new Client();
+			//to add client
+			
+	}
 		
 }
-		
